d695addc51d5b11ae1c6fdcbc71d9646460e0a85
c12msr <c12msr@cs.umu.se>
2017-01-22 00:14:51 +0100

    added class for storing files on internal or external memory

---
+++ app/src/main/java/se/umu/cs/c12msr/imagetimer/StorageHelper.java
@@ -0,0 +1,33 @@
+package se.umu.cs.c12msr.imagetimer;
+
+import android.os.Environment;
+
+/**
+ * Created by c12msr on 16-Jan-17.
+ */
+public class StorageHelper {
+
+    public StorageHelper(){
+
+    }
+
+
+    /* Checks if external storage is available for read and write */
+    public static boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public static boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+}
